801c4a5a544829053926e5bb5527b4b8a1e84d0c
龍門渕透華 <swearl@163.com>
2020-10-15 17:06:54 +0800

    修复页码bug

--- app/Controllers/Weibo.php
+++ app/Controllers/Weibo.php
@@ -13,7 +13,10 @@ class Weibo extends ResourceController {
     protected $modelName = 'App\Models\PostModel';
 
     public function index() {
-        $page = $this->request->getGet('page') || 1;
+        $page = (int) $this->request->getGet('page');
+        if ($page < 1) {
+            $page = 1;
+        }
         $data = $this->model->getPage($page);
         // echo $this->model->getLastQuery();
         // exit;
