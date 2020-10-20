673c00b4c8742f12914b78605ce16f9e11878ea4
龍門渕透華 <swearl@163.com>
2020-10-20 11:42:47 +0800

    添加样式

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import App from './App';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import 'reset-css';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app');
+createApp(App).use(store).use(router).mount('#app');
